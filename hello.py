a5fc9f15a9f3b842f0d418ca3d2728cd5dcef72a
Muhammad Umer <121258333+Muhammad1umer-tech@users.noreply.github.com>
2023-09-16 23:19:12 +0500

    Second

--- hello.py
+++ hello.py
@@ -1,12 +1,9 @@
 from flask import Flask, request
 app = Flask(__name__)
-
 @app.route('/', methods=['POST'])
 def index():
     request_data = request.get_json()
     name = request_data.get('name',"")
-    # model = keras.models.load_model("new_model.h5")
-    # print(model.summary())
     return f'You entered: {name}'
     
 
